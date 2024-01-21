8bc84befdf8df23de716fc638f1acda94ffecb80
naorb12 <133492118+naorb12@users.noreply.github.com>
2024-01-22 00:02:33 +0200

    Create General.cpp

---
+++ General.cpp
@@ -0,0 +1,25 @@
+#include "General.h"
+
+void gotoxy(int x, int y)
+{
+	HANDLE hConsoleOutput;
+	COORD dwCursorPosition;
+	cout.flush();
+	dwCursorPosition.X = x;
+	dwCursorPosition.Y = y;
+	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorPosition(hConsoleOutput, dwCursorPosition);
+
+	// Hide cursor
+	CONSOLE_CURSOR_INFO ConCurInf;
+	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	ConCurInf.dwSize = 10;
+	ConCurInf.bVisible = FALSE;
+	SetConsoleCursorInfo(hConsoleOutput, &ConCurInf);
+
+}
+
+void clrscr()
+{
+	system("cls");
+}
