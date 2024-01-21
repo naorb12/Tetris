cde83d5ce59e4e6f4847f1cb395be0af8e2dfce9
naorb12 <133492118+naorb12@users.noreply.github.com>
2024-01-22 00:00:11 +0200

    Create General.h

---
+++ General.h
@@ -0,0 +1,13 @@
+#ifndef _Genearal
+#define _Genearal
+
+#include <windows.h> // for gotoxy
+#include <process.h> // for system
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+void gotoxy(int, int);
+void clrscr();
+
+#endif // _Genearal
