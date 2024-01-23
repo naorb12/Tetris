f30848525d8887ba06684bdfa89e86203541e854
naorb12 <133492118+naorb12@users.noreply.github.com>
2024-01-23 20:06:37 +0200

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include "Board.h"
 
-using namespace std;
-
-
+// This function initialize the board to empty cells
 void Board::initBoard()
 {
     for (int i = 0; i < GameConfig::GAME_HEIGHT; i++) {
         for (int j = 0; j < GameConfig::GAME_WIDTH; j++) {
-            board[i][j] = ' '; // EMPTY
+            board[i][j] = GameConfig::EMPTY_CELL;
         }
     }
 }
 
-bool Board::isRowFull(int row) 
+// This function checks if a row is full in a board
+bool Board::isRowFull(const int row) const 
 {
     for (int col = 0; col < GameConfig::GAME_WIDTH; col++) 
     {
-        if (board[row][col] == ' ') 
+        if (board[row][col] == GameConfig::EMPTY_CELL)
         {  
             return false;
         }
@@ -25,7 +24,8 @@ bool Board::isRowFull(int row)
     return true;
 }
 
-void Board::shiftRowsDown(int startRow) 
+// This function shifts rows down 
+void Board::shiftRowsDown(const int startRow) 
 {
     for (int row = startRow; row > 0; row--)
     {
@@ -35,14 +35,15 @@ void Board::shiftRowsDown(int startRow)
         }
     }
 
-    // Clear the top row
+  
     for (int col = 0; col < GameConfig::GAME_WIDTH; col++)
     {
-        board[0][col] = ' ';  
+        board[0][col] = GameConfig::EMPTY_CELL;
     }
     clrscr();
 }
 
+// This function checks if rows are full in a board, and deletes them 
 void Board::clearRows(const Piece& piece) 
 {
     for (int i = 0; i < GameConfig::PIECE_SIZE; i++) 
@@ -51,32 +52,35 @@ void Board::clearRows(const Piece& piece)
         if (isRowFull(row))
         {
             shiftRowsDown(row);
-           // i = -1; // Restart loop to check for multiple full rows
+            i = -1;
         }
     }
 }
 
-
-void Board::printBoard(const int min)          
+// This function prints the board and its pieces
+void Board::printBoard(const int min) const          
 {
     for (int i = 0; i < GameConfig::GAME_HEIGHT; ++i) 
     {
         for (int j = 0; j < GameConfig::GAME_WIDTH; ++j)
         {
-            if (board[i][j] == '*')
+            if (board[i][j] == GameConfig::FILLED_CELL)
             {
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BACKGROUND_INTENSITY);
                 gotoxy(j + min + GameConfig::PIECE_X, i);
-                cout << '*';
+                cout << GameConfig::FILLED_CELL;
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
             }
         }
     }
 }
 
-void Board::insertPiece(Piece piece)   // This function is called after we know it hit (checkHit()), thus it is a simple one
+// This function inserts a piece into the board
+void Board::insertPiece(const Piece& piece)   
 {
     for (int i = 0; i < GameConfig::PIECE_SIZE; i++)
     {
-        board[piece.tetrimino[i].getY()][piece.tetrimino[i].getX()] = '*';
+        board[piece.tetrimino[i].getY()][piece.tetrimino[i].getX()] = GameConfig::FILLED_CELL;
     }
 }
 
