327a0a242020892d238547a1798d03f996acfafa
naorb12 <133492118+naorb12@users.noreply.github.com>
2024-01-23 20:09:31 +0200

    Update Piece.cpp

--- Piece.cpp
+++ Piece.cpp
@@ -1,6 +1,7 @@
 #include "Piece.h"
 #include "Board.h"
 
+// This function randomises a piece type for the piece
 void Piece::buildpiece()
 {
 	pieceType = rand() % GameConfig::NUMBER_OF_PIECE_TYPE;
@@ -69,13 +70,13 @@ void Piece::buildpiece()
     }
 }
 
-
+// This function cheks and moves a piece right on the board
 void Piece::moveRight(const Board& board)
 {
     bool checkRight = true;
     for (int i = 0; i < GameConfig::PIECE_SIZE; i++)
     {
-        if (tetrimino[i].getX() == GameConfig::GAME_WIDTH - 1 || board.board[tetrimino[i].getX() + 1][tetrimino[i].getY()] == '*')
+        if (tetrimino[i].getX() == GameConfig::GAME_WIDTH - 1 || board.board[tetrimino[i].getY()+1][tetrimino[i].getX() + 1] == GameConfig::FILLED_CELL)
             checkRight = false;
     }
     if (checkRight == true)
@@ -86,12 +87,14 @@ void Piece::moveRight(const Board& board)
         }
     }
 }
+
+// This function cheks and moves a piece left on the board
 void Piece::moveleft(const Board& board)
 {
     bool checkLeft = true;
     for (int i = 0; i < GameConfig::PIECE_SIZE; i++)
     {
-        if (tetrimino[i].getX() == 0 || board.board[tetrimino[i].getX() - 1][tetrimino[i].getY()] == '*')
+        if (tetrimino[i].getX() == 0 || board.board[tetrimino[i].getY()+1][tetrimino[i].getX() - 1] == GameConfig::FILLED_CELL)
             checkLeft = false;
     }
     if (checkLeft == true)
@@ -103,25 +106,26 @@ void Piece::moveleft(const Board& board)
     }
 }
 
-
+// This function checks and rotates a piece clockwise
 void Piece::rotateClockwise(const Board& board)
 {
     Point center = tetrimino[1];
 
-    // Check each block in the piece for boundary and collision conditions
     for (int i = 0; i < GameConfig::PIECE_SIZE; i++)
     {
         int newX = center.getY() - tetrimino[i].getY() + center.getX();
         int newY = tetrimino[i].getX() - center.getX() + center.getY();
 
         // Check boundary conditions
-        if (newX < 0 || newX >= GameConfig::GAME_WIDTH || newY < 0 || newY >= GameConfig::GAME_HEIGHT) {
-            return; 
+        if (newX < 0 || newX >= GameConfig::GAME_WIDTH || newY < 0 || newY >= GameConfig::GAME_HEIGHT) 
+        {
+            return;
         }
 
-        // Check collision with other pieces
-        if (board.board[newX][newY] == '*') {
-            return; 
+        // Check collision with other pieces in board
+        if (board.board[newY+1][newX+1] == GameConfig::FILLED_CELL)
+        {
+            return;
         }
     }
 
@@ -133,23 +137,25 @@ void Piece::rotateClockwise(const Board& board)
     }
 }
 
+// This function checks and rotates a piece counter clockwise
 void Piece::rotateCounterClockwise(const Board& board)
 {
     Point center = tetrimino[1];
 
-    // Check each block in the piece for boundary and collision conditions
     for (int i = 0; i < GameConfig::PIECE_SIZE; i++)
     {
         int newX = -1 * (tetrimino[i].getY() - center.getY()) + center.getX();
         int newY = (tetrimino[i].getX() - center.getX()) + center.getY();
 
         // Check boundary conditions
-        if (newX < 0 || newX >= GameConfig::GAME_WIDTH || newY < 0 || newY >= GameConfig::GAME_HEIGHT) {
-            return; 
+        if (newX < 0 || newX >= GameConfig::GAME_WIDTH || newY < 0 || newY >= GameConfig::GAME_HEIGHT) 
+        {
+            return;
         }
 
-        // Check collision with other pieces
-        if (board.board[newX][newY] == '*') {
+        // Check collision with other pieces in board
+        if (board.board[newY+1][newX+1] == GameConfig::FILLED_CELL)
+        {
             return;
         }
     }
