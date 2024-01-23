b674ddb7f8f3ff44b53a9bb924e30a05383a4960
naorb12 <133492118+naorb12@users.noreply.github.com>
2024-01-23 20:08:32 +0200

    Update General.cpp

--- General.cpp
+++ General.cpp
@@ -1,6 +1,7 @@
 #include "General.h"
 
-void gotoxy(int x, int y)
+// Used for printing the game
+void gotoxy(const int x,const int y)
 {
 	HANDLE hConsoleOutput;
 	COORD dwCursorPosition;
@@ -13,12 +14,13 @@ void gotoxy(int x, int y)
 	// Hide cursor
 	CONSOLE_CURSOR_INFO ConCurInf;
 	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-	ConCurInf.dwSize = 10;
+	ConCurInf.dwSize = DOWN;
 	ConCurInf.bVisible = FALSE;
 	SetConsoleCursorInfo(hConsoleOutput, &ConCurInf);
 
 }
 
+// Clears the screen
 void clrscr()
 {
 	system("cls");
