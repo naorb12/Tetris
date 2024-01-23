58576617517db53d371a732aa6175016ce11c6e6
naorb12 <133492118+naorb12@users.noreply.github.com>
2024-01-23 20:08:16 +0200

    Update GameConfig.h

--- GameConfig.h
+++ GameConfig.h
@@ -7,14 +7,13 @@ public:
 
 	static constexpr int GAME_WIDTH = 12;
 	static constexpr int GAME_HEIGHT = 18;
+	static constexpr char EMPTY_CELL = ' ';
+	static constexpr char FILLED_CELL = '*';
 	static constexpr int PIECE_SIZE = 4;
 	static constexpr int NUMBER_OF_PIECE_TYPE = 7;
-	static constexpr int BOTTOM = 22;
 
 	static constexpr int P2_X = 24;
 	static constexpr int PIECE_X = 1;
-
-	static const int COLORS[];
-	static const int NUM_OF_COLORS;
+	
 };
 #endif
