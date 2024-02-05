8fea40c2db43f9195c5859f0ed2f83d1b9e2ad23
naorb12 <133492118+naorb12@users.noreply.github.com>
2024-02-05 15:22:55 +0200

    Add files via upload

--- Board.h
+++ Board.h
@@ -4,19 +4,24 @@
 #include "GameConfig.h"
 #include "Piece.h"
 
+static constexpr int BOMB_RADIUS = 4;
 
 class Board 
 {
 	bool isRowFull(const int row) const;
 	void shiftRowsDown(const int startRow);
 
+	void performBomb(const Point& p, const int playerOffset);
+	void applyGravity(const int playerOffset);
+	int getMax(const int a, const int b) const { return (a > b) ? a : b; }
+	int getMin(const int a, const int b) const { return (a < b) ? a : b; }
 public:
 	char board[GameConfig::GAME_HEIGHT][GameConfig::GAME_WIDTH]; // move to private. use get() and set()
 
 	void initBoard();
 	void clearRows(const Piece& piece);
 	void printBoard(const int min) const;
-	void insertPiece(const Piece& piece);
+	void insertPiece(const Piece& piece, const int playerOffset);
 
 };
 
